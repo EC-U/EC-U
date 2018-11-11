04995892d656ce10683555814332f9dc1049debb
xzz2013 <xzz2013@gmail.com>
2018-11-11 20:14:06 +0800

    'find_date'

--- NLP/find_date.py
+++ NLP/find_date.py
@@ -4,14 +4,17 @@ from spilt_from_file import split,info
 t = split('ecnu_su.txt')
 old = info(t)[3]
 new = []
-pattern_date = '((\d{4}|\d{2})(-|/|.)\d{1,2}\3\d{1,2})|(\d{4}年\d{1,2}月\d{1,2}日)|(\d{1,2}月\d{1,2}日)|(\d{1,2}日)'
-pattren_time = '(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?|([1-24]\d时[0-60]\d分)|([1-24]\d时)'
-pattren_chn = '活动时间(.*)'
+pattern_date = r'((\d{4}|\d{2})(-|/|.)\d{1,2}\3\d{1,2})|(\d{4}年\d{1,2}月\d{1,2}日)|(\d{1,2}月\d{1,2}日)|(\d{1,2}日)'
+#pattern_date = re.compile(pattern_date)
+pattern_time = '(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?|([1-24]\d时[0-60]\d分)|([1-24]\d时)'
+pattern_time = pattern_time
+#pattern_time = re.compile(pattern_time)
+pattern_chn = '活动时间(.*)'
 for i in old:
-    x = re.search(pattren_chn,i)
+    x = re.search(pattern_chn,i)
     #print(d)
     #print(t)
     if x:
-        d = re.search(pattern_date, i)
-        t = re.search(pattren_time, i)
+        d = re.search(pattern_date,i)
+        t = re.search(pattern_time,i)
         print(d.group()+t.group())
