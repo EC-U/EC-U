7556501ebee6b528fffa2e899ece86a1264107aa
niuyuean <niuyuean99@sina.com>
2019-05-25 22:10:25 +0800

    update monitoring clipboard

--- 微信小程序/pages/create/create.js
+++ 微信小程序/pages/create/create.js
@@ -1,168 +1,41 @@
 Page({
   data: {
-    taskImportant:[1,2,3]
-  },
-  onLoad: function (option) {
-    var pageType = option.pageType || 'create';
-    var task;
-    var curDate;
-    if (pageType === 'create') {
-      var ms = option.ms || new Date().getTime();
-      curDate = moment(ms, "x");
-      task = {
-        title: "新建任务",
-        important: "一般",
-        date: moment(ms, 'x').format("YYYY-MM-DD")
-      };
-    } else {
-      var key = option.key;
-      if (key) {
-        task = taskService.get({ key: key });
-        curDate = moment(task.startTimeMs);
-      }
-    }
-    this.setData({ curDate: curDate });
-    var taskTime = _fn.getTaskTime(task);
-    var taskImportant = ['一般', '重要'];
-    this.setData({
-      task: task,
-      taskTime: taskTime,
-      taskImportant: taskImportant,
-      pageType: pageType,
-    });
+    title:'',
+    date:'',
+    time:'',
+    position:'',
+    sheet_num: 0
   },
   onChangeTitle: function (e) {
-    var task = _fn.getCurTask();
-    var value = e.detail.value;
-    task.title = value;
-    var addBtnStatus = value.length > 0 ? "active" : "inActive";
     this.setData({
-      addBtnStatus: addBtnStatus
+      title: e.detail.value
     });
   },
-  onChangeImportant: function (e) {
-    var task = _fn.getCurTask();
-    var value = e.detail.value;
-    task.important = _fn.mapImportant(value);
-    this.setData({ task: task });
-  },
-  onChangeStartTime: function (e) {
-    var task = _fn.getCurTask();
-    var value = e.detail.value;
-    console.log(e);
-    task.startTime = value;
-    var taskTime = _fn.getTaskTime();
-    utils.getPageData().setData({ taskTime: taskTime });
-
-  },
-  onChangeEndTime: function (e) {
-    var task = _fn.getCurTask();
-    var value = e.detail.value;
-    task.endTime = value;
-    var taskTime = _fn.getTaskTime();
-    utils.getPageData().setData({ taskTime: taskTime });
-  },
-  onChangeAllDay: function (e) {
-    var task = _fn.getCurTask();
-    var value = e.detail.value;
-    if (value === true) {
-      task.startTime = "00:00";
-      task.endTime = "24:59";
-    } else {
-      task.startTime = null;
-      task.endTime = null;
-    }
-    var taskTime = _fn.getTaskTime();
-    utils.getPageData().setData({ taskTime: taskTime });
-  },
-  saveTask: function (e) {
-    var task = _fn.getCurTask();
-    taskService.creat(task);
-    wx.navigateBack({
-      delta: 1
+  onChangeDate: function (e) {
+    this.setData({
+      date: e.detail.value
     });
   },
-  updateTask: function (e) {
-    var taskKey = e.target.dataset.taskkey;
-    var task = _fn.getCurTask();
-    taskService.update({
-      key: taskKey,
-      val: task
-    });
-    wx.navigateBack({
-      delta: 1
+  onChangeTime: function (e) {
+    this.setData({
+      time: e.detail.value
     });
   },
-  cancelTask: function (e) {
-    wx.navigateBack({
-      delta: 1
+  onChangePosition: function (e) {
+    this.setData({
+      position: e.detail.value
     });
   },
-  removeTask: function () {
-    var task = _fn.getCurTask();
-    var dayMs = dateService.getDateKey(moment(task.date).valueOf());
-    taskService.delete({
-      dayMs: dayMs,
-      taskKey: task.key
-    }, function () {
-      wx.navigateBack({
-        delta: 1
-      });
+  onChangeNum: function (e) {
+    this.setData({
+      sheet_num: e.detail.value
     });
+  },
+  formSubmit: function (e) {
+    
+    wx.request({
+      url: 'http://122.152.233.115:8080/v1/text/',
+      method: 'POST'
+    })
   }
 });
-_fn = {
-  getCurTask: function () {
-    var task = utils.getPageData().data.task;
-    // console.log(task);
-    return task;
-  },
-  getTaskTime: function (task) {
-    task = task || {};
-    var now = utils.getPageData().data.curDate;
-    var dateStr = task.date || now.format('YYYY-MM-DD');
-    var curTask = _fn.getCurTask() || task || {};
-    var startTime = curTask.startTime ? curTask.startTime : now.format("HH:mm");
-    var startTimeMoment = moment(dateStr + " " + startTime);
-    var endTimeMoment;
-    if (curTask.endTime) {
-      endTimeMoment = moment(dateStr + " " + curTask.endTime);
-      if (!endTimeMoment.isAfter(startTimeMoment)) {
-        endTimeMoment = moment(startTimeMoment);
-        endTime = endTimeMoment.add(1, 'h').format("HH:mm");
-      } else {
-        endTime = curTask.endTime;
-      }
-    } else {
-      endTimeMoment = moment(startTimeMoment);
-      endTimeMoment = endTimeMoment.add(1, 'h');
-      endTime = endTimeMoment.format("HH:mm");
-    }
-    curTask.startTimeMs = startTimeMoment.valueOf();
-    curTask.endTimeMs = endTimeMoment.valueOf();
-    curTask.startTime = startTime;
-    curTask.endTime = endTime;
-
-
-    var startTimeBeginLimit = now.format("HH:mm");
-    var endTimeBeginLimit = startTime;
-
-    var taskTime = {
-      startTime: startTime,
-      endTime: endTime,
-      startTimeBeginLimit: startTimeBeginLimit,
-      endTimeBeginLimit: endTimeBeginLimit,
-      date: dateStr
-
-    };
-    return taskTime;
-  },
-  mapImportant: function (key) {
-    var map = {
-      "0": "一般",
-      "1": "重要"
-    };
-    return map[key];
-  }
-
-};
--- 微信小程序/pages/test/test.js
+++ 微信小程序/pages/test/test.js
@@ -1,93 +1,18 @@
 // pages/test/test.js
 Page({
-<<<<<<< HEAD
-
-=======
->>>>>>> e9101a13a9b87ec450312021a7cded5e002e2278
-  /**
-   * 页面的初始数据
-   */
   data: {
-<<<<<<< HEAD
-      text:''
-=======
-    text: "one",
-    btnText:"nothing"
->>>>>>> e9101a13a9b87ec450312021a7cded5e002e2278
+    clipdata:"nochange"
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-<<<<<<< HEAD
-
-=======
-    this.setData({btnText:"nothing"})
->>>>>>> e9101a13a9b87ec450312021a7cded5e002e2278
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-<<<<<<< HEAD
-  copyTBL: function (e) {
-    var self = this;
-    wx.setClipboardData({
-      data: self.data.taokouling,
-      success: function (res) {
-        console.log(res.data);
-          self.setData({text: res.data})
+  watch: function(e){
+    var that = this
+    wx.getClipboardData({
+      success(res) {
+        that.setData({
+          clipdata: res.data
+        })
+        //console.log(res.data)
       }
     })
-=======
-  btnClick:function(){
-    this.setData({btnText:"cliked!"})
->>>>>>> e9101a13a9b87ec450312021a7cded5e002e2278
+    
   }
 })
